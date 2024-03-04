6a57ba4ebb416d200b81dddb491a46edaba945a7
jessica garcia <93187222+JG38@users.noreply.github.com>
2024-03-04 13:40:32 -0500

    first commit

--- Hw_Day4_Wk2.py
+++ Hw_Day4_Wk2.py
@@ -1,46 +1,7 @@
-cart = {
-    'eggs': 6,
-    'bread': 2,
-}
-
-def show():
-    if len(cart) == 0:
-        print('Your cart is empty')
-    print(cart)
-    for k, v in cart.items():
-        print(f'There are {v} {k} in your cart')
-
-def add():
-    item = input('What item do you want to add?')
-    quantity = input('How many do you want to buy?')
-    cart[item] = int(quantity)
-    print(f'You now have {quantity} {item} in your cart')
-
-def delete():
-    item = input('What item do you want to remove?')
-    if item in cart:
-        del cart[item]
-        print(f'{item} has been removed from your cart')
-    else:
-        print(f'{item} is not in your cart')
-
-def clear():
-    global cart
-    cart = {}
-    print('Your cart is now empty')
-
-def runner():
-    while True:
-        menu = input('What do you want to do? Show/Add/Delete/Clear/Quit')
-        if menu.lower() == 'show':
-            show()
-        elif menu.lower() == 'add':
-            add()
-        elif menu.lower() == 'delete':
-            delete()
-        elif menu.lower() == 'clear':
-            clear()
-        elif menu.lower() == 'quit':
-            return
-        else:
-            print('Not a valid option')
+class StringManipulator:
+    def __init__(self)
+        self.string = input("Hello World!")
+    
+    def print_String(self)
+        print(self.string)
+        print(self.string.upper())
