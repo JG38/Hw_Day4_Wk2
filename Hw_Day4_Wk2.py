b0e583e3b8b3f01a9ddb2e1e46d77edb17d8c572
jessica garcia <93187222+JG38@users.noreply.github.com>
2024-03-06 15:05:48 -0500

    Main

--- Hw_Day4_Wk2.py
+++
@@ -1,7 +0,0 @@
-class StringManipulator:
-    def __init__(self)
-        self.string = input("Hello World!")
-    
-    def print_String(self)
-        print(self.string)
-        print(self.string.upper())
